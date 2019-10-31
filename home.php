c194965618c72e2b9a6ad61e0777e5fface8c1ad
Danny <danny.larrea.argudo@gmail.com>
2019-10-31 18:36:04 +0100

    Subir home.php

---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME · Casal</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="cerrar_ses"><p>Usuario | <a href="index.php">Cerrar sesión</a></p></div>
+
+	<header id="main-header">
+		
+		<a id="logo-header" href="incidencias.php" class="imagenEveris" />CASAL</a>
+ 
+		<nav class="cab">
+			<ul>
+				<li><a href="home.php">HOME</a></li>
+				<li><a href="Reservar.php">HISTORIAL</a></li>
+				<li><a href="historial.php">MIS RESERVAS</a></li>
+			</ul>
+		</nav>
+
+		<a class="ePRIM" href="#">
+			 Incidencias
+		</a>
+ 
+	</header>
+	<br><br><br>
+	<button class="boton">Reservar</button>
+
+
+
+</body>
+</html>
