58e572ac89003464239949a4c3f79af1d505451a
Importantelio <55807889+Importantelio@users.noreply.github.com>
2019-11-05 15:24:00 +0100

    Estructura general de la página y estilos

---
+++ home.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME · Casal</title>
+	<meta charset="utf-8">
+	<!--<link rel="stylesheet" type="text/css" href="style.css">-->
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+
+	<div class="cerrar_ses"><p class="parr_ses">Usuario | <a href="index.php">Cerrar sesión</a></p></div>
+
+	<header id="main-header">
+		
+		<a id="logo-header" href="incidencias.php" class="imagenEveris" /><img src="./web/img/logo.png" width="200px"></a>
+ 
+		<nav class="cab">
+			<ul>
+				<li><a href="home.php">HOME</a></li>
+				<li><a href="historial.php">HISTORIAL</a></li>
+				<li><a href="mis_reservas.php">MIS RESERVAS</a></li>
+			</ul>
+		</nav>
+
+		
+		<button class="incidencias">Incidencias</button>
+ 
+	</header>
+	<div class="div_img"><img src="https://cdn-e360.s3-sa-east-1.amazonaws.com/entrevista-que-debo-hacer-para-que-mi-hijo-se-adapte-al-colegio-2-large-Ph9B1hnpZd.jpg" class="imagen"></div>
+	<br><br><br>
+	<div class="reservar_todo">
+		<p class="reservar_parr">
+			Reservas Disponibles
+		</p>
+		<div class="reservar_cont">
+			<div class="res_div">
+				<div class="res_i_r">
+					<img src="https://img.elo7.com.br/product/zoom/1CC68BD/par-de-quadro-para-sala-casal-colorido-70x140.jpg" class="img_res">
+					<p class="parr_res">Sala Multidisciplinar</p>
+				</div>
+				<button class="boton_res">Reservar</button>
+			</div>
+
+<div class="res_div">
+				<div class="res_i_r">
+					<img src="https://img.elo7.com.br/product/zoom/1CC68BD/par-de-quadro-para-sala-casal-colorido-70x140.jpg" class="img_res">
+					<p class="parr_res">Sala Multidisciplinar</p>
+				</div>
+				<button class="boton_res">Reservar</button>
+			</div>
+<div class="res_div">
+				<div class="res_i_r">
+					<img src="https://img.elo7.com.br/product/zoom/1CC68BD/par-de-quadro-para-sala-casal-colorido-70x140.jpg" class="img_res">
+					<p class="parr_res">Sala Multidisciplinar</p>
+				</div>
+				<button class="boton_res">Reservar</button>
+			</div>
+<div class="res_div">
+				<div class="res_i_r">
+					<img src="https://img.elo7.com.br/product/zoom/1CC68BD/par-de-quadro-para-sala-casal-colorido-70x140.jpg" class="img_res">
+					<p class="parr_res">Sala Multidisciplinar</p>
+				</div>
+				<button class="boton_res">Reservar</button>
+			</div>
+<div class="res_div">
+				<div class="res_i_r">
+					<img src="https://img.elo7.com.br/product/zoom/1CC68BD/par-de-quadro-para-sala-casal-colorido-70x140.jpg" class="img_res">
+					<p class="parr_res">Sala Multidisciplinar</p>
+				</div>
+				<button class="boton_res">Reservar</button>
+			</div>
+<div class="res_div">
+				<div class="res_i_r">
+					<img src="https://img.elo7.com.br/product/zoom/1CC68BD/par-de-quadro-para-sala-casal-colorido-70x140.jpg" class="img_res">
+					<p class="parr_res">Sala Multidisciplinar</p>
+				</div>
+				<button class="boton_res">Reservar</button>
+			</div>
+<div class="res_div">
+				<div class="res_i_r">
+					<img src="https://img.elo7.com.br/product/zoom/1CC68BD/par-de-quadro-para-sala-casal-colorido-70x140.jpg" class="img_res">
+					<p class="parr_res">Sala Multidisciplinar</p>
+				</div>
+				<button class="boton_res">Reservar</button>
+			</div>
+
+
+
+
+		</div>
+
+	</div>
+
+	<br><br><br>
+	<div></div>
+
+<?php
+?>
+
+</body>
+</html>
